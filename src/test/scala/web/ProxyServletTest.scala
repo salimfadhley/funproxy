add5d62bd6c5cab1920d406c529dc6b41b331456
salimfadhley <slpg500a>
2016-02-05 18:35:33 +0000

    Trying to make fixtures work

--- src/test/scala/web/ProxyServletTest.scala
+++ src/test/scala/web/ProxyServletTest.scala
@@ -3,48 +3,53 @@ package web
 /**
   * Created by salim on 03/02/2016.
   */
+
 import org.scalatest.FunSuiteLike
 import org.scalatra.test.scalatest.ScalatraSuite
+class FixtureServlet extends JSONServlet {
+  var hitCount: Int = 0
+
+  get("/status") {
+    val ret = s"OK! $hitCount"
+    hitCount += 1
+    ret
+  }
+
+  get("/foo") {
+    val baseUrl = request.getRequestURL.toString
+    List("a", "b", "c").map((s: String) =>
+      EndpointInfo(baseUrl + "/" + s)
+    )
+  }
+
+
+}
+
 class ProxyServletTest extends ScalatraSuite with FunSuiteLike {
 
 
 
-  class FixtureServlet extends JSONServlet {
-
-    get("/fixtures") {
-      "hello"
-    }
-
-    get("/foo") {
-      val baseUrl = request.getRequestURL.toString
-      List("a", "b", "c").map((s: String) =>
-        EndpointInfo(baseUrl + "/" + s)
-      )
-    }
-
-  }
-
-  addServlet(classOf[ProxyServlet], "/fixtures/*")
+  addServlet(classOf[FixtureServlet], "/fixtures/*")
   addServlet(classOf[ProxyServlet], "/proxy/*")
 
   test("get status") {
     get("/proxy/status") {
-      status should equal (200)
-      body should include ("OK!")
+      status should equal(200)
+      body should include("OK!")
     }
   }
 
   test("get foo") {
     get("/proxy/foo") {
-      status should equal (200)
+      status should equal(200)
     }
   }
 
   test("verify fixtures") {
     get("/fixtures/foo") {
-      status should equal (200)
-//      body should include ("a" +
-//        "")
+      status should equal(200)
+      //      body should include ("a" +
+      //        "")
     }
   }
 
